565a9477c714f43934e82fb0eb78317555b0a7d7
Orlando Bloor <orlandobloor678@gmail.com>
2025-11-13 21:00:04 +1300

    index fix

--- api/index.py
+++ api/index.py
@@ -2,8 +2,12 @@ import os
 import sys
 from pathlib import Path
 
-project_root = Path(__file__).parent.parent
+current = Path(__file__).resolve()
+project_root = current.parent.parent
+monorepo_root = project_root.parent
+
 sys.path.insert(0, str(project_root))
+sys.path.insert(0, str(monorepo_root))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
