49dba1f514a868ef0c663e5005dd580643f6e9fc
Orlando Bloor <orlandobloor678@gmail.com>
2025-11-13 20:57:39 +1300

    index fix

--- api/index.py
+++ api/index.py
@@ -7,11 +7,5 @@ sys.path.insert(0, str(project_root))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
-import django
-django.setup()
-
 from django.core.asgi import get_asgi_application
 application = get_asgi_application()
-
-async def handler(request, context):
-    return await application(request, context)
