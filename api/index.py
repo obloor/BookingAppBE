f6b947eeb0771386ea6c55fd7e83ccb8139364d0
Orlando Bloor <orlandobloor678@gmail.com>
2025-11-13 21:05:21 +1300

    index fix

--- api/index.py
+++ api/index.py
@@ -1,15 +1,15 @@
+# api/index.py
+
 import os
 import sys
 from pathlib import Path
 
 current = Path(__file__).resolve()
 project_root = current.parent.parent
-monorepo_root = project_root.parent
-
 sys.path.insert(0, str(project_root))
-sys.path.insert(0, str(monorepo_root))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
-from django.core.asgi import get_asgi_application
-application = get_asgi_application()
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
