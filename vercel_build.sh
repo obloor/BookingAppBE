07c5c1c135a925254e6dc6e711d58b6c74df78f9
Orlando Bloor <orlandobloor678@gmail.com>
2025-11-13 20:27:30 +1300

    vercel tweaks

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#!/bin/bash
+set -e  # Exit on error
+
+# Install Python dependencies
+echo "Installing dependencies..."
 pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+# Run migrations
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Build completed successfully!"
