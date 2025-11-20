693fc6c3c3013694f1b524974b6db5dd07b9618e
Orlando Bloor <orlandobloor678@gmail.com>
2025-11-20 19:10:23 +1300

    cors fix

--- app/settings.py
+++ app/settings.py
@@ -100,7 +100,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://reservation-app-sepia.vercel.app",
+    "https://reservation-frontend.vercel.app/",
 ]
 
 if DEBUG:
